23a7eb473019440dcd58588dca7aefb41fdf11a6
shima <shima11@users.noreply.github.com>
2016-11-12 22:45:30 +0900

    タップエフェクトを削除

--- FillButton/FillButton.swift
+++ FillButton/FillButton.swift
@@ -15,7 +15,6 @@ import UIKit
     @IBInspectable var subColor: UIColor = UIColor.white
     @IBInspectable var borderWidth: CGFloat = 1.0
     
-    private let tapEffectView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 1))
 
     override func draw(_ rect: CGRect) {
         print("draw rect")
@@ -51,37 +50,13 @@ import UIKit
         
         self.setTitleColor(mainColor, for: .normal)
         self.setTitleColor(subColor, for: .highlighted)
-
-
-        // 円を描画
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.fillColor = mainColor.cgColor
-        shapeLayer.path = UIBezierPath(ovalIn: tapEffectView.bounds).cgPath
-        tapEffectView.layer.addSublayer(shapeLayer)
-        tapEffectView.isHidden = true
         
-        addSubview(tapEffectView)
     }
     
     // タップしたとき
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        if let point = touches.first?.location(in: self) {
-            tapEffectView.frame.origin = point
-            
-            tapEffectView.alpha = 1.0
-            tapEffectView.isHidden = false
-            tapEffectView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            
-            UIView.animate(withDuration: 0.4,
-                                       animations: {
-                                        self.tapEffectView.alpha = 1.0
-                                        self.tapEffectView.transform = CGAffineTransform(scaleX: 200.0, y: 200.0)
-            }) { finished in
-                self.tapEffectView.isHidden = true
-                self.tapEffectView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            }
-        }
+    
     }
     
     // ハイライト時に色を反転させる
